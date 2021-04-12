ea62cb21c91e989ff11a60e9db0b6013b5f747b7
jespi116 <jespi116@fiu.edu>
2021-04-11 21:57:59 -0400

    descriptions added to projects, more responsive portfolio component, added another project

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,42 +11,56 @@ function Portfolio(){
             technologies: 'HTML/CSS',
             link: "https://jespi116.github.io/run-buddy/",
             repo: "https://github.com/jespi116/run-buddy",
-            src: "runbuddy.png"
-        },
-        {
-            name: 'Home Plate',
-            technologies: 'HTML/CSS/JS/SQL',
-            link: "https://homeplate-12345.herokuapp.com/",
-            repo: "https://github.com/jespi116/Home-Plate",
-            src: "homeplate.png"
-        },
-        {
-            name: 'Weather Dashboard',
-            technologies: 'HTML/CSS/JS/API',
-            link: "https://jespi116.github.io/weather-dashboard/",
-            repo: "https://github.com/jespi116/weather-dashboard",
-            src: "Weather.png"
-        },
-        {
-            name: 'The VirtualYardSale',
-            technologies: 'MERN Stack',
-            link: "https://vysale.herokuapp.com/",
-            repo: "https://github.com/jespi116/vysale",
-            src: "vys.png"
+            src: "runbuddy.png",
+            desc: 'Front End page that displays some serious CSS skills!'
         },
         {
             name: 'Shop Shop',
             technologies: 'MERN Stack',
             link: "https://enigmatic-island-98076.herokuapp.com/",
             repo: "https://github.com/jespi116/shop-shop",
-            src: "shop-shop.png"
+            src: "shop-shop.png",
+            desc: 'Full Stack e-commerce site using mongoDB and Redux'
+        },
+        {
+            name: 'Home Plate',
+            technologies: 'HTML/CSS/JS/SQL',
+            link: "https://homeplate-12345.herokuapp.com/",
+            repo: "https://github.com/jespi116/Home-Plate",
+            src: "homeplate.png",
+            desc: 'Full Stack e-commerce site for Sporting goods that uses MySQL!'
+        },
+        {
+            name: 'Pizza Hunt',
+            technologies: 'HTML/CSS/JS/NOSQL',
+            link: 'https://pacific-lowlands-31234.herokuapp.com/',
+            repo: 'https://github.com/jespi116/pizza-hunt',
+            src: 'pizza-hunt.png',
+            desc: 'Full Stack Webpage for pizza enthusiasts to find and share new pizza recipes!'
+        },
+        {
+            name: 'Weather Dashboard',
+            technologies: 'HTML/CSS/JS/API',
+            link: "https://jespi116.github.io/weather-dashboard/",
+            repo: "https://github.com/jespi116/weather-dashboard",
+            src: "Weather.png",
+            desc: 'Front End Webpage for displaying weather forecasts in the user\'s city of choice powered by openweathermap.org\'s weather api.'
+        },
+        {
+            name: 'The VirtualYardSale',
+            technologies: 'MERN Stack',
+            link: "https://vysale.herokuapp.com/",
+            repo: "https://github.com/jespi116/vysale",
+            src: "vys.png",
+            desc: 'Full Stack PWA meant for users to buy and sell used items!'
         }
     ])
 
     return(
         <section className='mx-5'>
             <h1>Portfolio</h1>
-            <div className="flex-row">
+            <h3>hover over an image to see an overview of the project</h3><h3> Click on the title or GitHub icon to be taken to the active site or the repository of that project!</h3>
+            <div className="flex-row flex-start">
                 {projects.map((project) => (
                     <Projects
                         project = { project }
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,19 +4,24 @@ import icon from '../../assets/imgs/github.png'
 //component for displaing the mapped project array passed from the portfolio component
 
 function Projects({project}){
-    const { name, technologies, link, repo, src } = project;
+    const { name, technologies, link, repo, src, desc } = project;
 
     return(
-        <div className="project-container">
-            <img className="project-img" src={require(`../../assets/imgs/${src}`).default} alt={name} />
-            <div className="project-text">
-                <div>
-                    <a href={link} className='project-link bold'>{name}</a>
-                    <a href={repo} className='repo-link'>
-                        <img src={icon} alt='repo link' />
-                    </a>
+        <div className="container">
+            <div className="project-container">
+                <img className="project-img" src={require(`../../assets/imgs/${src}`).default} alt={name} />
+                <div className="project-text">
+                    <div>
+                        <a href={link} className='project-link bold'>{name}</a>
+                        <a href={repo} className='repo-link'>
+                            <img src={icon} alt='repository link' />
+                        </a>
+                        
+                    </div>
+                    <p className="tech bold">{technologies}</p>
+                    <p className="desc">{desc}</p>
                 </div>
-                <p className="tech bold">{technologies}</p>
+                
             </div>
         </div>
     );
